a010143f928013ea39170f4a33e62a231cc5d206
Sonja Gurtner <sonja.gurtner@gmail.com>
2022-11-15 17:41:17 +0100

    improve script

--- macros-copy.sh
+++ macros-copy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-#.S file into which macros should be copied
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+#file into which macros should be copied
 MYFILE=$1
 
 #copy new instruction macros into file
-cat /home/sonja/GitHub/riscv-oisc-macros/riscv-container/OISC/all_macros/macros.S ${MYFILE} > /tmp/temp2.S
+cat ${SCRIPTPATH}/riscv-container/OISC/all_macros/macros.S ${MYFILE} > /tmp/temp2.S
 mv /tmp/temp2.S ${MYFILE}
